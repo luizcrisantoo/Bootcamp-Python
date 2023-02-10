ed56a7a811edf38eae2cc6e6c795ee4cfa0420da
Luiz Eduardo Cajueiro Crisanto <87085913+luizcrisantoo@users.noreply.github.com>
2023-02-10 16:17:35 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,170 @@
+# Exercise1
+number = int(input('enter a number: '))
+print(f'{number}')
+
+# Exercise2
+number1 = float(input('enter a number: '))
+print(f'{number1}')
+
+# Exercise3
+number3 = int(input('Sum: enter a number: '))
+number4 = int(input('Sum: enter a number: '))
+number5 = int(input('Sum: enter a number: '))
+print(f' Sum: {number3 + number4 + number5}')
+
+# Exercise4
+number6 = float(input ('Pow: enter a number: '))
+print(f' Pow: {number6 ** 2}')
+
+# Exercise5
+number7 = float(input ('Div: enter a number: '))
+print(f' Div: {number7 / 5}')
+
+# Exercise6
+Celsius = float(input ('Enter the temperature in Celsius: '))
+print(f' Fahrenheit = {Celsius * (9.0/5.0) + 32.0}')
+
+# Exercise7
+Fahrenheit = float(input ('Enter the temperature in Fahrenheit: '))
+print(f' Celsius = {(5.0 * (Fahrenheit - 32.0))/9}')
+
+# Exercise8
+Kelvin = float(input ('Enter the temperature in Kelvin: '))
+print(f' Celsius = {Kelvin - 273.15}')
+
+# Exercise9
+Celsius1 = float(input ('Enter the temperature in Celsius: '))
+print(f' Kelvin = {Celsius1 + 273.15}')
+
+# Exercise10
+K = float(input ('Enter the km/h: '))
+print(f' M/s = {K/3.6}')
+
+# Exercise11
+M = float(input ('Enter the m/s: '))
+print(f' Km/h = {M*3.6}')
+
+# Exercise12
+Mi = float(input ('Enter the M: '))
+print(f' Km = {Mi*1.61}')
+
+# Exercise13
+Km = float(input ('Enter the Km: '))
+print(f' Mi = {Km/1.61}')
+
+# Exercise14
+G = float(input ('Enter the Degress: '))
+print(f' Radians = {(G * 3.14)/180}')
+
+# Exercise15
+R = float(input ('Enter the Radians: '))
+print(f' Degress = {(R*180)/3.14}')
+
+# Exercise16
+P = float(input ('Enter the inches: '))
+print(f' Centimeters = {P*2.54}')
+
+# Exercise17
+C = float(input ('Enter the Centimeters: '))
+print(f' Iches = {C/2.54}')
+
+# Exercise18
+m3 = float(input ('Enter the Cubic meters: '))
+print(f' liters = {1000 * m3}')
+
+# Exercise19
+l = float(input ('Enter the liters: '))
+print(f' Cubic meters = {l/1000}')
+
+# Exercise20
+Kg = float(input ('Enter the Kg: '))
+print(f' Pounds = {Kg/0.45}')
+
+# Exercise21
+Pounds = float(input ('Enter the Pounds: '))
+print(f' Kg = {Pounds*0.45}')
+
+# Exercise22
+Yards = float(input ('Enter the Yards: '))
+print(f' Meters = {0.91 * Yards}')
+
+# Exercise23
+Meters = float(input ('Enter the Meters: '))
+print(f' Yards = {0.91 * Meters}')
+
+# Exercise24
+m2 = float(input ('Enter the Square meters: '))
+print(f' acres = {m2 * 0.000247}')
+
+# Exercise25
+acres = float(input ('Enter the acres: '))
+print(f' Square meters = {acres * 4048.58 }')
+
+# Exercise26
+SquareMeters = float(input ('Enter the Square meters: '))
+print(f' Acre = {SquareMeters * 0.0001 }')
+
+# Exercise27
+Acre = float(input ('Enter the Acre: '))
+print(f' Square meters = {Acre * 10000 }')
+
+# Exercise28
+numbers1 = float(input('enter a number: '))
+numbers2 = float(input('enter a number: '))
+numbers3 = float(input('enter a number: '))
+print(f' Sum: {(numbers1**2) + (numbers2**2) + (numbers3**2)}')
+
+# Exercise29
+note1 = float(input('enter the note: '))
+note2 = float(input('enter the note: '))
+note3 = float(input('enter the note: '))
+note4 = float(input('enter the note: '))
+print(f' Average: {(note1 + note2 + note3 + note4)/4}')
+
+# Exercise30
+real = float(input('enter the real value R$: '))
+print(f' Dollars: {real/5.24}')
+
+# Exercise31
+numbers = float(input('enter the number: '))
+print(f' Successor: {numbers + 1} ,Predecessor {numbers-1}')
+
+# Exercise32
+numbers10 = float(input('enter the number: '))
+print(f' {((numbers10*3) + 1) + ((numbers10*2) -1)} ')
+
+# Exercise33
+L = float(input('enter the L value: '))
+print(f' m2: {L**2}')
+
+# Exercise34
+r = float(input('enter the R value: '))
+print(f' Circlem2: {r*3.141592}')
+
+# Exercise35
+import math
+A = float(input('enter the A value: '))
+B = float(input('enter the B value: '))
+Sum = (A**2) + (B**2)
+Hipo = math.sqrt(Sum)
+print(f' Hipo: {Hipo}')
+
+# Exercise36
+r = float(input('enter the R value: '))
+h = float(input('enter the H value: '))
+print(f' V3: {(r**2)*3.141592*h}')
+
+# Exercise37
+Value = float(input('enter the R$ value: '))
+print(f' Value Total: {Value *0.12}')
+
+# Exercise38
+Wage = float(input('enter the R$ Wage value: '))
+print(f' New Wage: {Wage + (Wage/4)}')
+
+# Exercise39
+first = 780.000*0.46
+second = 421.200*0.32
+third = 286.416
+print(f' R$: {first},R$: {second},R$: {third}')
+
